b5fe9afdefd07c1964df54644aa14dcb3f882ed0
joonho lee <goodfluence@naver.com>
2021-08-31 08:43:18 +0900

    빌드 에러 수정 2)

--- components/Common/container.tsx
+++ components/Common/container.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import Head from "next/head";
-import { CMS_NAME } from "@/lib/constants";
+import { BLOG_NAME } from "@/lib/constants";
 import PostType from "@/types/post";
 import CommentScript from "@/components/Common/commentScript";
 
@@ -20,7 +20,7 @@ const Container = ({ post, children }: Props) => {
         <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
         <script>hljs.highlightAll()</script>
         <title>
-          {post.title} | Next.js Blog Example with {CMS_NAME}
+          {post.title} | {BLOG_NAME}
         </title>
         <meta property="og:image" content={post.ogImage.url} />
       </Head>
