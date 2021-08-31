4a41415eb9002d7fe88a42a15dd56eeed1da95c9
joonho lee <goodfluence@naver.com>
2021-08-31 20:19:07 +0900

    done 4)

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,6 @@ export const BLOG_NAME = "heyJoon blog.";
 export const HOME_OG_IMAGE_URL =
   "https://og-image.vercel.app/Next.js%20Blog%20Starter%20Example.png?theme=light&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg";
 export const CSSDIR = process.cwd() + "/_posts/CSS/";
-export const JAVASCRIPTDIR = process.cwd() + "/_posts/Javascript/";
+export const JAVASCRIPTDIR = process.cwd() + "/_posts/JavaScript/";
 export const HTMLDIR = process.cwd() + "/_posts/HTML/";
 export const NODEJSDIR = process.cwd() + "/_posts/Nodejs/";
